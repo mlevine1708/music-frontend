706f93a30164704ef30c407c3e7bdd3baeb5fb17
Micah Levine <micah.levine@outlook.com>
2021-05-13 15:36:27 -0400

    started reducers

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import userReducer from "./reducers.userReducer";
 
 const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
