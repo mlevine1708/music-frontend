7ce94e1cf3fce06bfe89ece830ef10746150f991
Micah Levine <micah.levine@outlook.com>
2021-06-02 09:47:54 -0400

    posts container

---
+++ src/containers/PostsContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PostInput from "../components/PostInput";
+import Posts from "../components/Posts";
+
+class PostsContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <PostInput user={this.props.user} />
+        <br />
+        <Transactions transactions={this.props.user && this.props.user.posts} />
+      </div>
+    );
+  }
+}
+
+export default PostsContainer;
---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { fetchUsers } from "../actions/fetchUsers";
+import Users from "../components/Users";
+import User from "../components/User";
+import UserInput from "../components/UserInput";
+import NavBar from "../components/NavBar";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/users/new" component={UserInput} />
+          <Route
+            path="/users/:id"
+            render={(routerProps) => (
+              <User {...routerProps} users={this.props.users} />
+            )}
+          />
+          <Route
+            path="/users"
+            render={(routerProps) => (
+              <Users {...routerProps} users={this.props.users} />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps, { fetchUsers })(UsersContainer);
