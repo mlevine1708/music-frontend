d9ad4c620839cf178050ede241b6fe8c8140275e
Micah Levine <micah.levine@outlook.com>
2021-06-02 08:54:33 -0400

    changed list to post

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Posts from "./components/Posts";
+import "./App.css";
 
-class App extends React.Component {
-  /* componentDidMount() {
-    fetch("http://localhost:3000/api/v1/users/1", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }*/
-  render() {
-    return <div className="App">App</div>;
-  }
-}
+const App = (props) => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      genre: "Rock",
+      kind: "Albums",
+    },
+    {
+      id: 2,
+      genre: "Pop",
+      kind: "Songs",
+    },
+    {
+      id: 3,
+      genre: "Jazz",
+      kind: "Artists",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <Header />
+      <Posts posts={posts} />
+    </div>
+  );
+};
+
 export default App;
